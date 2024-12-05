ce6e5a4e79a6bf6c9b61353cdeb445a1e9069b15
Dương Minh Thông <duongminhthong1608@gmail.com>
2024-12-05 17:15:12 +0700

    Update Câu 14
    Hoàn Thành sắp xếp Giảng Viên

--- Câu 14.cpp
+++ Câu 14.cpp
@@ -42,7 +42,50 @@ class Teacher : public Human {
 		
 		void OutputTeacher() {
 			Human::Output();
-			cout<<"Department subject: " <<departmentSubject;
-			cout<<"Teaching subject: " <<teachingSubject;
+			cout<<"Department subject: " <<departmentSubject <<endl ;
+			cout<<"Teaching subject: " <<teachingSubject <<endl;
+		}
+		
+		string get_departmentSubject() {
+			return departmentSubject;
 		}
 };
+
+void Arrange(Teacher teacher[], int n) {
+	
+	for (int i = 0; i < n-1; i++) {
+		for (int j = 0; j < n; j++) {
+			if (teacher[i].get_departmentSubject() > teacher[j].get_departmentSubject()) {
+				swap(teacher[i], teacher[j]);
+			}
+		}
+	}
+	
+}
+
+int main() {
+	
+	int n;
+	cout<<"Enter number of teacher: ";
+	cin>>n;
+	
+	Teacher teacher[20];
+	for (int i = 0; i < n; i++) {
+        teacher[i].InputTeacher();
+    }
+    
+    cout<<"------ Information ------" <<endl;
+    for (int i = 0; i < n; i++) {
+    	teacher[i].OutputTeacher();
+    }
+    
+    
+    Arrange(teacher, n);
+    cout<<"------ List after arranged: ------" <<endl;
+    
+	for (int i = 0; i < n; i++) {
+    	teacher[i].OutputTeacher();
+    }
+    
+    return 0;
+}
